b5c27ba3f39ebbabda7bb29aedfcad6936d0aa7b
Kozlov Maksym <kozlovmaksinn@gmail.com>
2024-01-17 11:41:54 +0200

    JWT Token authentication
    Added JWT token authentication

--- dzencodeproject/settings.py
+++ dzencodeproject/settings.py
@@ -32,11 +32,13 @@ INSTALLED_APPS = [
     "rest_framework",
     "commentsapp",
     "drf_spectacular",
+    "rest_framework_simplejwt",
 ]
 
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.BasicAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
